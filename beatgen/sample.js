27ed064966f827715c0f8bca30380321027462b6
Kyle Dhillon <kdhillon@princeton.edu>
2016-05-07 19:19:23 -0400

    s

--- beatgen/sample.js
+++ beatgen/sample.js
@@ -17,7 +17,10 @@ function loadAudio(object, url) {
 }
 
 function playSound(object, semitones, gain) {
-    if (object.s == null) return;
+    if (object.s == null) {
+        console.log("object.s is null");   
+        return;
+    }
     if (object.s.buffer == null) return;
     if (object.playing) {
         // console.log("stopping");
