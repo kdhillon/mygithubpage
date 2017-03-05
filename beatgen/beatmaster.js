dddb1cb9e9318442d236435128a6d5088859df78
Kyle Dhillon <kdhillon@princeton.edu>
2017-03-05 13:43:14 -0500

    last before switching to static

--- beatgen/beatmaster.js
+++ beatgen/beatmaster.js
@@ -82,7 +82,6 @@ function onTouch() {
 	measureEvery = beatEvery * 4;
 	subBeatEvery = beatEvery / 4;
 
-	
 	length32seconds = measureEvery * 2 * measuresToLoad;
 	// Length in seconds 
 	console.log("length 32 seconds " + length32seconds);
